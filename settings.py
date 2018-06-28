27909cb8bca07ee0a1a2eff4bda011cc8cc26329
Edward van Kuik <edward@nitric.co.za>
2018-06-28 17:53:03 +0200

    test

--- settings.py
+++ settings.py
@@ -1,1 +1,6 @@
-weburl = "http://lemonchat.localhost"
+weburl = "http://lemonchat.localhost"\
+
+try:
+    import settings_local.py  # Override your deployment settings in here.
+except Exception as e:
+    pass
