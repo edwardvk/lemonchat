3d589203ec9e9bfacca368536feebe1a33b5b666
Nicholas Spagnoletti <nicholas@nitric.co.za>
2018-07-31 15:27:11 +0200

    changes

--- root.py
+++ root.py
@@ -1,7 +1,9 @@
 import os
 import cherrypy
-import mako.template
+import mako.lookup
 import api.root
+
+TEMPLATES = mako.lookup.TemplateLookup(['templates'])
 
 # sys.stdout = sys.stderr
 # currentdir = os.path.dirname(os.path.realpath(__file__))
@@ -15,14 +17,11 @@ class Root(object):
     def index(self, user_id, agent=0, template='index'):
         assert template in ('index', 'chat')
         # @TODO If agent, then asset that user_id is actually an agent.
-<<<<<<< HEAD
-        template = mako.template.Template(filename=template + ".mako.html")
-=======
-        template = mako.template.Template(filename=os.path.join("templates", "index.mako.html"))
->>>>>>> 64f55c37e42c38d9a68c5620c102a917841fdc26
+        template = TEMPLATES.get_template(template + ".mako.html")
         return template.render(user_id=user_id, agent=agent)
 
     api = api.root.Root()
+
 
 root = Root()
 application = cherrypy.Application(root)
