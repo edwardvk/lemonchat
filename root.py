ba6a65a19cf0e252854f668c27f4e9b3e2b16600
Edward van Kuik <edward@nitric.co.za>
2018-08-05 16:42:04 +0200

    Added cookies

--- root.py
+++ root.py
@@ -3,6 +3,8 @@ import cherrypy
 import mako.lookup
 import api.root
 import json
+import random
+import re
 from cryptography.fernet import Fernet
 import settings
 
@@ -15,14 +17,42 @@ TEMPLATES = mako.lookup.TemplateLookup(['templates'])
 # cherrypy.config.update({'environment': 'test_suite'})
 
 
+def pickname():
+    words = open("/usr/share/dict/words").read().split("\n")
+    badwords = open("badwords.txt").read().split("\n")
+    while True:
+        word = random.choice(words)
+        if word in badwords:
+            continue
+        if re.match("^[a-z]+$", word):
+            break
+    word += str(random.randint(10, 99))
+    word = word[0].upper() + word[1:]
+    return word
+
+
 class Root(object):
     @cherrypy.expose
-    def index(self, loginauth, template='index'):
+    def index(self, loginauth=None, template='index'):
         assert template in ('index', 'chat')
         f = Fernet(settings.secret)  # lemongroup
-        input = json.loads(f.decrypt(loginauth.encode()))
-        user_id = input['user_id']
-        agent = input['agent']
+
+        if not loginauth:
+            # Look for cookies.
+            if cherrypy.request.cookie.get('loginauth'):
+                loginauth = str(cherrypy.request.cookie.get('loginauth').value)
+
+        if not loginauth:
+            # Assume an anonymous user, create a random user name for them.
+            user_id = pickname()
+            agent = 0
+            
+            loginauth = str(f.encrypt(json.dumps({'user_id': user_id, 'agent': agent}).encode()), "utf-8")
+            cherrypy.response.cookie['loginauth'] = str(loginauth)
+        else:
+            input = json.loads(f.decrypt(loginauth.encode()))
+            user_id = input['user_id']
+            agent = input['agent']
 
         template = TEMPLATES.get_template(template + ".mako.html")
         return template.render(user_id=user_id, agent=agent)
